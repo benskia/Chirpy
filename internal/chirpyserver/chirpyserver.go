1ee6d6d2d51e19e34947c23044f63d689d0845de
benskia <benski.abraham@gmail.com>
2024-06-21 13:36:49 -0500

    add apiConfig, handlers /healthz /metrics /reset; restructure website

--- internal/chirpyserver/chirpyserver.go
+++ internal/chirpyserver/chirpyserver.go
@@ -3,22 +3,62 @@ package chirpyserver
 import (
 	"log"
 	"net/http"
+	"strconv"
 )
 
-const (
-	localHost   string = "localhost:8080"
-	contentPath string = "./website/content/"
-	assetsPath  string = "./website/assets/"
-)
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	cfg.fileserverHits++
+	return next
+}
+
+func (cfg *apiConfig) metricsHandle(w http.ResponseWriter, _ *http.Request) {
+	w.Header()["Content-Type"] = []string{"text/plain; charset=utf-8"}
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte(strconv.Itoa(cfg.fileserverHits)))
+	if err != nil {
+		log.Println("/metrics failed to write body data")
+	}
+}
+
+func (cfg *apiConfig) resetHandle(w http.ResponseWriter, _ *http.Request) {
+	cfg.fileserverHits = 0
+	w.Header()["Content-Type"] = []string{"text/plain; charset=utf-8"}
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte(strconv.Itoa(cfg.fileserverHits)))
+	if err != nil {
+		log.Println("/reset failed to write body data")
+	}
+}
 
 func StartChirpyServer() {
+	const (
+		localHost   string = "localhost:8080"
+		websitePath string = "./website/"
+	)
+
 	mux := http.NewServeMux()
+	apiCfg := apiConfig{}
+	handler := http.FileServer(http.Dir(websitePath))
+	mux.Handle("/app/*", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(handler)))
+	mux.HandleFunc("/healthz", readinessHandle)
+	mux.HandleFunc("/metrics", apiCfg.metricsHandle)
+	mux.HandleFunc("/reset", apiCfg.resetHandle)
 
-	mux.Handle("/", http.FileServer(http.Dir(contentPath)))
-	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir(assetsPath))))
-
-	srv := http.Server{Handler: mux, Addr: localHost}
+	srv := &http.Server{Handler: mux, Addr: localHost}
 	if err := srv.ListenAndServe(); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func readinessHandle(w http.ResponseWriter, _ *http.Request) {
+	w.Header()["Content-Type"] = []string{"text/plain; charset=utf-8"}
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte("OK"))
+	if err != nil {
+		log.Println("/healthz failed to write body data")
 	}
 }
