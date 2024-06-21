b24eefcef416f2655532dcb976e1a88456f7d77e
benskia <benski.abraham@gmail.com>
2024-06-21 14:54:43 -0500

    update routing; add metrics template and update handler

--- internal/chirpyserver/chirpyserver.go
+++ internal/chirpyserver/chirpyserver.go
@@ -1,52 +1,61 @@
 package chirpyserver
 
 import (
+	"html/template"
 	"log"
 	"net/http"
-	"strconv"
+)
+
+const (
+	localHost    string = "localhost:8080"
+	websitePath  string = "./website/"
+	templatePath string = "./web/"
 )
 
 type apiConfig struct {
 	fileserverHits int
 }
 
+func NewApiConfig() *apiConfig {
+	return &apiConfig{0}
+}
+
+func (cfg *apiConfig) GetHits() int {
+	return cfg.fileserverHits
+}
+
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	cfg.fileserverHits++
-	return next
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
 }
 
 func (cfg *apiConfig) metricsHandle(w http.ResponseWriter, _ *http.Request) {
-	w.Header()["Content-Type"] = []string{"text/plain; charset=utf-8"}
+	w.Header()["Content-Type"] = []string{"text/html; charset=utf-8"}
 	w.WriteHeader(http.StatusOK)
-	_, err := w.Write([]byte(strconv.Itoa(cfg.fileserverHits)))
-	if err != nil {
-		log.Println("/metrics failed to write body data")
-	}
+	t, _ := template.ParseFiles(templatePath + "metrics.html")
+	t.Execute(w, cfg)
 }
 
 func (cfg *apiConfig) resetHandle(w http.ResponseWriter, _ *http.Request) {
 	cfg.fileserverHits = 0
 	w.Header()["Content-Type"] = []string{"text/plain; charset=utf-8"}
 	w.WriteHeader(http.StatusOK)
-	_, err := w.Write([]byte(strconv.Itoa(cfg.fileserverHits)))
+	_, err := w.Write([]byte("Fileserver hit counter has been reset."))
 	if err != nil {
 		log.Println("/reset failed to write body data")
 	}
 }
 
 func StartChirpyServer() {
-	const (
-		localHost   string = "localhost:8080"
-		websitePath string = "./website/"
-	)
-
+	apiCfg := NewApiConfig()
+	handler := http.StripPrefix("/app", http.FileServer(http.Dir(websitePath)))
 	mux := http.NewServeMux()
-	apiCfg := apiConfig{}
-	handler := http.FileServer(http.Dir(websitePath))
-	mux.Handle("/app/*", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(handler)))
-	mux.HandleFunc("/healthz", readinessHandle)
-	mux.HandleFunc("/metrics", apiCfg.metricsHandle)
-	mux.HandleFunc("/reset", apiCfg.resetHandle)
+	mux.Handle("GET /app/*", apiCfg.middlewareMetricsInc(handler))
+	mux.HandleFunc("GET /api/healthz", readinessHandle)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.metricsHandle)
+	mux.HandleFunc("GET /api/reset", apiCfg.resetHandle)
 
 	srv := &http.Server{Handler: mux, Addr: localHost}
 	if err := srv.ListenAndServe(); err != nil {
