91d8054668b3a0cdbf06c04a7ef8b5a083e94627
benskia <benski.abraham@gmail.com>
2024-06-22 17:38:10 -0500

    add validate_chirp handle

--- internal/chirpyserver/chirpyserver.go
+++ internal/chirpyserver/chirpyserver.go
@@ -1,6 +1,7 @@
 package chirpyserver
 
 import (
+	"encoding/json"
 	"html/template"
 	"log"
 	"net/http"
@@ -32,7 +33,7 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) metricsHandle(w http.ResponseWriter, _ *http.Request) {
-	w.Header()["Content-Type"] = []string{"text/html; charset=utf-8"}
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	t, _ := template.ParseFiles(templatePath + "metrics.html")
 	t.Execute(w, cfg)
@@ -40,12 +41,68 @@ func (cfg *apiConfig) metricsHandle(w http.ResponseWriter, _ *http.Request) {
 
 func (cfg *apiConfig) resetHandle(w http.ResponseWriter, _ *http.Request) {
 	cfg.fileserverHits = 0
-	w.Header()["Content-Type"] = []string{"text/plain; charset=utf-8"}
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	_, err := w.Write([]byte("Fileserver hit counter has been reset."))
 	if err != nil {
-		log.Println("Error handling /reset : ", err)
+		log.Println("Error handling /reset: ", err)
 	}
+}
+
+func validateChirpHandle(w http.ResponseWriter, r *http.Request) {
+	type failResponse struct {
+		Error string `json:"error"`
+	}
+	type successResponse struct {
+		Valid bool `json:"valid"`
+	}
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	fail := failResponse{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Println("Error decoding parameters: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		fail.Error = "Something went wrong"
+		dat, err := json.Marshal(fail)
+		if err != nil {
+			log.Println("Error marshalling JSON: ", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Write(dat)
+		return
+	}
+
+	if len(params.Body) > 140 {
+		log.Println("Chirp is too long")
+		w.WriteHeader(http.StatusBadRequest)
+		fail.Error = "Chirp is too long"
+		dat, err := json.Marshal(fail)
+		if err != nil {
+			log.Println("Error marshalling JSON: ", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Write(dat)
+		return
+	}
+
+	success := successResponse{true}
+	dat, err := json.Marshal(success)
+	if err != nil {
+		log.Println("Error marshalling JSON: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(dat)
 }
 
 func readinessHandle(w http.ResponseWriter, _ *http.Request) {
@@ -53,7 +110,7 @@ func readinessHandle(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	_, err := w.Write([]byte("OK"))
 	if err != nil {
-		log.Println("Error handling /healthz : ", err)
+		log.Println("Error handling /healthz: ", err)
 	}
 }
 
@@ -65,6 +122,7 @@ func StartChirpyServer() {
 	mux.HandleFunc("GET /api/healthz", readinessHandle)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.metricsHandle)
 	mux.HandleFunc("GET /api/reset", apiCfg.resetHandle)
+	mux.HandleFunc("POST /api/validate_chirp", validateChirpHandle)
 
 	srv := &http.Server{Handler: mux, Addr: localHost}
 	if err := srv.ListenAndServe(); err != nil {
---
+++ scripts/restTest.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+curl -v http://localhost:8080/app/
+
+curl -v --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"body":"A short chirp."}' \
+  http://localhost:8080/api/validate_chirp
+
+curl -v --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"body":"This is a longer chirp. Chirps cannot contain more than 140 characters, so we'\''re gonna keep typing until we have at least 141 characters. This should do it."}' \
+  http://localhost:8080/api/validate_chirp
