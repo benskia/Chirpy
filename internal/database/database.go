7bd2c1d6e93d0475ff943acbb6201613d7f3a389
benskia <benski.abraham@gmail.com>
2024-06-25 16:26:29 -0500

    update CreateChirp

--- internal/database/database.go
+++ internal/database/database.go
@@ -2,7 +2,6 @@ package database
 
 import (
 	"encoding/json"
-	"log"
 	"os"
 	"sort"
 	"sync"
@@ -31,10 +30,18 @@ func NewDB(path string) (*DB, error) {
 }
 
 func (db *DB) CreateChirp(body string) (Chirp, error) {
-	return Chirp{
-		ID:   0,
+	dbStructure, err := db.loadDB()
+	if err != nil {
+		return Chirp{}, err
+	}
+	index := db.nextIndex(dbStructure)
+	newChirp := Chirp{
+		ID:   index,
 		Body: body,
-	}, nil
+	}
+	dbStructure.Chirps[index] = newChirp
+	err = db.writeDB(dbStructure)
+	return newChirp, err
 }
 
 func (db *DB) GetChirps(body string) ([]Chirp, error) {
@@ -63,21 +70,33 @@ func (db *DB) ensureDB() error {
 }
 
 func (db *DB) loadDB() (DBStructure, error) {
+	db.mux.RLock()
+	defer db.mux.RUnlock()
 	dbStructure := DBStructure{}
 	f, err := os.ReadFile(db.path)
 	if err != nil {
 		return dbStructure, err
 	}
-	err = json.Unmarshal(f, dbStructure)
+	err = json.Unmarshal(f, &dbStructure)
 	return dbStructure, err
 }
 
 func (db *DB) writeDB(dbStructure DBStructure) error {
 	db.mux.Lock()
+	defer db.mux.Unlock()
 	dbToWrite, err := json.Marshal(dbStructure)
-	db.mux.Unlock()
 	if err != nil {
 		return err
 	}
 	return os.WriteFile(db.path, dbToWrite, 0644)
+}
+
+func (db *DB) nextIndex(dbStructure DBStructure) int {
+	next := 0
+	for i := range dbStructure.Chirps {
+		if i > next {
+			next = i
+		}
+	}
+	return next + 1
 }
